d8d3aebce2d9e88cfd97a6c7253c32578bf160a4
Aubrey Malabie <aubrey@aftarobot.com>
2023-09-10 11:40:19 +0200

    🍎initial commit after repo move

---
+++ src/test/java/com/boha/kasietransie/KasietransieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boha.kasietransie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasietransieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
